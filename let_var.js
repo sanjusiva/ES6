d00d23554ec5bf5f6a005914c9dd3d77c4ce2466
sanjusiva <96572583+sanjusiva@users.noreply.github.com>
2021-12-30 12:32:03 +0530

    let and var

---
+++ let_var.js
@@ -0,0 +1,12 @@
+//Let & var
+
+if(true)
+{
+  let china="Chinese";
+  var world="English";
+  console.log(`Language in China is ${china}`);//prints output
+  console.log(`Language in world is ${world}`);//prints output
+}
+
+console.log(`Language in world is ${world}`);//prints output
+console.log(`Language in China is ${china}`);//produces error
